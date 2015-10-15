68e0d4f7707407ac462e940e53185a375343ecd9
crazyyy <crazyyy@gmail.com>
2015-10-15 19:21:09 +0300

    page

--- wordpress/wp-content/themes/wp-casino/page.php
+++ wordpress/wp-content/themes/wp-casino/page.php
@@ -1,18 +1,12 @@
 <?php get_header(); ?>
-  <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="layout-content layout-content-light_top">
 
-      <h1 class="page-title inner-title"><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-      <?php edit_post_link(); ?>
+    <h1 class="header-h1"><?php the_title(); ?></h1>
+    <div class="game-desc">
+      <div class="footer_pre clear_fix">
+        <?php the_content(); ?>
+      </div>
+    </div><!-- game-desc -->
 
-    </article>
-  <?php endwhile; else: // If 404 page error ?>
-    <article>
-
-      <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
-
-    </article>
-  <?php endif; ?>
-<?php get_sidebar(); ?>
+  </div><!-- layout-content layout-content-light_top -->
 <?php get_footer(); ?>
