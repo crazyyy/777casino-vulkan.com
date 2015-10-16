0b2830e17e9648a85db6efa38517080d467a7683
crazyyy <crazyyy@gmail.com>
2015-10-16 22:58:19 +0300

    marksist

--- wordpress/wp-content/themes/wp-casino/category.php
+++ wordpress/wp-content/themes/wp-casino/category.php
@@ -11,16 +11,16 @@
 
     <div class="games">
       <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-      <li>
-        <a rel="nofollow" class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <li class="newlister">
           <?php if ( has_post_thumbnail()) :
             the_post_thumbnail('medium');
           else: ?>
             <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
           <?php endif; ?>
           <div class="title-game"><?php the_title(); ?></div>
-        </a>
-      </li>
+          <a rel="nofollow" class="play-game" href="<?php the_field('link'); ?>" title="Играть <?php the_title(); ?>">Играть</a>
+          <a rel="nofollow" class="view-demo" href="<?php the_permalink(); ?>" title="Демо <?php the_title(); ?>">Демо</a>
+        </li>
       <?php endwhile; else: ?>
       <?php endif; ?>
     </div>
--- wordpress/wp-content/themes/wp-casino/page-home.php
+++ wordpress/wp-content/themes/wp-casino/page-home.php
@@ -9,15 +9,15 @@
         <div class="games">
           <?php query_posts("showposts=50"); ?>
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-          <li>
-            <a rel="nofollow" class="feature-img" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-              <?php if ( has_post_thumbnail()) :
-                the_post_thumbnail('medium');
-              else: ?>
-                <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
-              <?php endif; ?>
-              <div class="title-game"><?php the_title(); ?></div>
-            </a>
+          <li class="newlister">
+            <?php if ( has_post_thumbnail()) :
+              the_post_thumbnail('medium');
+            else: ?>
+              <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+            <?php endif; ?>
+            <div class="title-game"><?php the_title(); ?></div>
+            <a rel="nofollow" class="play-game" href="<?php the_field('link'); ?>" title="Играть <?php the_title(); ?>">Играть</a>
+            <a rel="nofollow" class="view-demo" href="<?php the_permalink(); ?>" title="Демо <?php the_title(); ?>">Демо</a>
           </li>
         <?php endwhile; endif; ?>
         <?php wp_reset_query(); ?>
